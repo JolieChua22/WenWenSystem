09ee33ed63228177be0f67201a8a8d7e1aa72838
Kok Soon <koksoon1106@gmail.com>
2024-11-25 12:11:25 +0800

    [Finishes #188609813]

--- src/wenwen2/Wenwen2.java
+++ src/wenwen2/Wenwen2.java
@@ -14,7 +14,7 @@ public class Wenwen2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("test4");
+        System.out.println("test5");
     }
     
 }
