273c4a00ebc30a067910d4f68ba9366c535f8e5d
JolieChua22 <joliechua123@gmail.com>
2024-11-29 17:28:34 +0800

    [commit #188623310] clean script file

--- agile_assignment/js/script.js
+++ agile_assignment/js/script.js
@@ -1,28 +1,6 @@
 document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent default form submission
 
-  // const name = document.getElementById("name").value;
-  // const email = document.getElementById("email").value;
-
-  // fetch("http://localhost:3000/submit", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({ name, email }),
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     if (data.error) {
-  //       alert(`Error: ${data.error}`);
-  //     } else {
-  //       alert("Data submitted successfully!");
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //     alert("Failed to submit data.");
-  //   });
    });
 
   
